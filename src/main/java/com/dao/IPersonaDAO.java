7f3bac752bc859ef935582b8f52cef201e64bc10
Rafael Ferrer <rferrerb710@gmail.com>
2017-10-20 12:53:29 +0200

    compleata personaDAO

--- src/main/java/com/dao/IPersonaDAO.java
+++ src/main/java/com/dao/IPersonaDAO.java
@@ -17,9 +17,17 @@ public interface IPersonaDAO {
 	/**
 	 * Metodo para buscar una Persona.
 	 * @param persona, objeto de clase Persona para la busqueda.
-	 * @return 
+	 * @return La Persona por la que se realizo la busqueda.
 	 */
 	public Persona get(Persona persona);
+	/**
+	 * Metodo para guardar una nueva Persona o guardar los cambios si existe en la BD. 
+	 * @param persona, objeto de la clase Persona que se desea guardar o actualizar en la BD.
+	 */
 	public void saveoOrUpdate(Persona persona);
+	/**
+	 * Metodo para eliminar una persona
+	 * @param persona, objeto de la clase Persona que se va a eliminar en el BD,
+	 */
 	public void delete(Persona persona);
 }
