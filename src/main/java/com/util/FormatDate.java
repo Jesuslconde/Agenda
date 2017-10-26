6ac91ffcf9a03ea0d4adca25d541b0db81ab84c8
admin <admin@pwc1>
2017-10-26 14:28:40 +0200

    ya paro

--- src/main/java/com/util/FormatDate.java
+++ src/main/java/com/util/FormatDate.java
@@ -1,8 +1,5 @@
 package com.util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class FormatDate {
 	public static String fechacat(String fmal) {
@@ -12,10 +9,5 @@ public class FormatDate {
 		String part2 = parts[2]; 
 		String fbien = part2+ "/" + part1 + "/" + part0;
 		return fbien;
-	}
-	public static Date spToUsDate(Date date) {
-		DateFormat formatter= new SimpleDateFormat("dd/MM/yyyy HH:mm:ss Z");
-		  System.out.println(formatter.format(date));
-		  return date;
 	}
 }
